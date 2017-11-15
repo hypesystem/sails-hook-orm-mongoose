5c099b96706ede168db84b9275fe106becdef9a8
Anders Borch <anders@cyborch.com>
2017-11-15 13:55:51 +0100

    Added mockgoose support.

--- index.js
+++ index.js
@@ -42,7 +42,9 @@ module.exports = function (sails) {
 
         // These optional connection options are passed in to mongoose when `.connect()` is called.
         // See http://mongoosejs.com/docs/connections.html for a full list of available options.
-        connectionOpts: {}
+        connectionOpts: {},
+
+        mockgoose: false
 
       }
 
@@ -118,6 +120,11 @@ module.exports = function (sails) {
         // Expose `sails.mongoose`.
         // (note that it's important to do this _before_ the other stuff below so that it is accessible for use in custom
         //  `constructSchema` interceptor functions, in case any of those are being used)
+        if (sails.config.mongoose.mockgoose) {
+          var Mockgoose = require('mockgoose').Mockgoose;
+          mockgoose = new Mockgoose(mongoose);
+        }
+
         sails.mongoose = mongoose;
 
 
@@ -127,138 +134,149 @@ module.exports = function (sails) {
         //
         // We save a reference to this Mongoose db connection, and listen for the first error emitted from it.
         // (any subsequent error events emitted that are emitted will crash the server!)
-        var opts = sails.config.mongoose.connectionOpts || {};
-        opts.useMongoClient = true;
-        var dbConnection = sails.mongoose.connect(sails.config.mongoose.uri, opts);
-        dbConnection.once('error', function (err){
 
-          // Handle weird (and very unlikely) case where Mongoose emits an error event without sending an Error instance as the event data.
-          if ( !err ) {
-            throw new Error('Unrecognized Mongoose connection error (connection emitted error event with no data)');
-          }
-          else if ( !_.isObject(err) || !_.isString(err.message) ) {
-            throw new Error('Unrecognized Mongoose connection error (connection emitted error event with non-Error data: `'+err+'`)');
-          }
+        var connectToDb = function () {
+          var opts = sails.config.mongoose.connectionOpts || {};
+          opts.useMongoClient = true;
+          var dbConnection = sails.mongoose.connect(sails.config.mongoose.uri, opts);
+          dbConnection.once('error', function (err){
 
-          // Negotiate error in order to offer troubleshooting tips for most common error condition
-          // (i.e. mongo server isn't running on the specified connection URI)
-          //
-          // After trying all of these:
-          // ============================================================
-          // console.log('keys:',_.keys(err));
-          // console.log('code:',err.code);
-          // console.log('name:',err.name);
-          // console.log('type:',err.type);
-          // console.log('errno:',err.errno);
-          // console.log('syscall:',err.syscall);
-          // console.log('error:',err.error);
-          // console.log('original:',err.original);
-          // console.log('Object.getOwnPropertyNames:',Object.getOwnPropertyNames(err));
-          // console.log('constructor:',err.constructor);
-          // console.log('constructor.name:',err.constructor.name);
-          // console.log('prototype:',err.prototype);
-          // console.log('Object.keys:',Object.keys(err));
-          // console.log('regex:',err.message.match(/ECONNREFUSED/));
-          // console.log('__proto__:',err.__proto__);
-          // ============================================================
-          //
-          // ...it became clear that the best way to negotiate this error is with a regexp.
-          //
-          // If this is an ECONNREFUSED error, then there is probably something going on with the database config.
-          if ( err.message.match(/ECONNREFUSED/) ) {
-            var troubleshootingMsg = 'Could not connect to Mongo database (@`'+sails.config.mongoose.uri+'`).\nMake sure the Mongo database at that connection URI is running and that the connection URI is correct.\nPlease also verify that any additional connection options (`sails.config.mongoose.connectionOpts`) are valid.\nFor help, see http://mongoosejs.com/docs/connections.html.';
-            err.message = troubleshootingMsg;
-            err.stack = troubleshootingMsg + '\n\nError details:\n==============================\n' + err.stack + '\n';
-            err.code = 'ECONNREFUSED';
-            return cb(err);
-          }
+            // Handle weird (and very unlikely) case where Mongoose emits an error event without sending an Error instance as the event data.
+            if ( !err ) {
+              throw new Error('Unrecognized Mongoose connection error (connection emitted error event with no data)');
+            }
+            else if ( !_.isObject(err) || !_.isString(err.message) ) {
+              throw new Error('Unrecognized Mongoose connection error (connection emitted error event with non-Error data: `'+err+'`)');
+            }
 
-          // Otherwise, this is some miscellaneous error we don't recognize, so just send it through.
-          return cb(err);
-        });
-        //
-        // We also listen for the `open` event, which indicates that we were able to successfully connect to the database.
-        dbConnection.once('open', function (){
-
-          // Load model definitions using the module loader.
-          // Returned `modules` are case-insensitive, using filename to determine identity.
-          // (This calls out to the `moduleloader` hook, which uses `sails-build-dictionary` and `includeall`
-          //  to `require` and collate the relevant code for these modules-- also adding the appropriate `globalId`
-          //  property.)
-          sails.log.verbose('Loading the app\'s models from `%s`...', sails.config.paths.models);
-          sails.modules.loadModels(function modulesLoaded(err, modules) {
-            if (err) {
+            // Negotiate error in order to offer troubleshooting tips for most common error condition
+            // (i.e. mongo server isn't running on the specified connection URI)
+            //
+            // After trying all of these:
+            // ============================================================
+            // console.log('keys:',_.keys(err));
+            // console.log('code:',err.code);
+            // console.log('name:',err.name);
+            // console.log('type:',err.type);
+            // console.log('errno:',err.errno);
+            // console.log('syscall:',err.syscall);
+            // console.log('error:',err.error);
+            // console.log('original:',err.original);
+            // console.log('Object.getOwnPropertyNames:',Object.getOwnPropertyNames(err));
+            // console.log('constructor:',err.constructor);
+            // console.log('constructor.name:',err.constructor.name);
+            // console.log('prototype:',err.prototype);
+            // console.log('Object.keys:',Object.keys(err));
+            // console.log('regex:',err.message.match(/ECONNREFUSED/));
+            // console.log('__proto__:',err.__proto__);
+            // ============================================================
+            //
+            // ...it became clear that the best way to negotiate this error is with a regexp.
+            //
+            // If this is an ECONNREFUSED error, then there is probably something going on with the database config.
+            if ( err.message.match(/ECONNREFUSED/) ) {
+              var troubleshootingMsg = 'Could not connect to Mongo database (@`'+sails.config.mongoose.uri+'`).\nMake sure the Mongo database at that connection URI is running and that the connection URI is correct.\nPlease also verify that any additional connection options (`sails.config.mongoose.connectionOpts`) are valid.\nFor help, see http://mongoosejs.com/docs/connections.html.';
+              err.message = troubleshootingMsg;
+              err.stack = troubleshootingMsg + '\n\nError details:\n==============================\n' + err.stack + '\n';
+              err.code = 'ECONNREFUSED';
               return cb(err);
             }
 
-            try {
+            // Otherwise, this is some miscellaneous error we don't recognize, so just send it through.
+            return cb(err);
+          });
+          //
+          // We also listen for the `open` event, which indicates that we were able to successfully connect to the database.
+          dbConnection.once('open', function (){
 
-              // Instantiate Mongoose schemas for each model definition (running custom `constructSchema` functions if provided)
-              var schemas = _.reduce(modules, function (memo, def, identity) {
-
-                // Validate `schema` from model def (if omitted, default it to `{}`)
-                if ( _.isUndefined(def.schema) ){
-                  def.schema = {};
-                }
-                if ( !_.isObject(def.schema) || _.isArray(def.schema) ) {
-                  throw new Error('Invalid `schema` provided in model (`'+identity+'`).  If provided, `schema` must be a dictionary.');
-                }
-
-                // If no `constructSchema` interceptor function was provided, just new up a Mongoose Schema by passing in `schema` from the model def.
-                if ( _.isUndefined(def.constructSchema) ) {
-                  memo[identity] = new sails.mongoose.Schema(def.schema);
-                }
-                // If `constructSchema` interceptor function WAS provided, run it to get the Schema instance.
-                else if ( _.isFunction(def.constructSchema) ) {
-                  try {
-                    memo[identity] = def.constructSchema(def.schema, sails);
-                  }
-                  catch (e) {
-                    e.message = 'Encountered an error when running `constructSchema` interceptor provided for model (`'+identity+'`). Details:\n' + e.message;
-                    e.stack = 'Encountered an error when running `constructSchema` interceptor provided for model (`'+identity+'`). Details:\n' + e.stack;
-                    throw e;
-                  }
-                }
-                else {
-                  throw new Error('Invalid `constructSchema` interceptor provided in model (`'+identity+'`).  If provided, `constructSchema` must be a function.');
-                }
-
-
-                return memo;
-              }, {});
-
-              // Now generate Model constructors from those schemas and expose references to them as `sails.models[identity]`.
-              //
-              // We also set `globalId` and `identity` directly on each Mongoose model.
-              // (this is for consistency with the standard ORM hook, and improved compatibility with any code in other
-              //  community hooks which relies on these properties existing)
-              // Set `globalId` and `identity` directly on the Mongoose model
-              sails.models = _.reduce(schemas, function (memo, mongooseSchemaInstance, identity) {
-                memo[identity] = sails.mongoose.model(identity, mongooseSchemaInstance);
-                memo[identity].globalId = modules[identity].globalId;
-                memo[identity].identity = identity;
-                return memo;
-              }, {});
-
-
-              // If configured to do so, also expose instantiated models as global variables.
-              // (using `globalId` to expose these models process-wide)
-              if ( _.isObject(sails.config.globals) && sails.config.globals.models ) {
-                _.each(sails.models, function eachInstantiatedModel(Model, identity) {
-                  // Expose the Model as a global variable.
-                  global[Model.globalId] = Model;
-                });
+            // Load model definitions using the module loader.
+            // Returned `modules` are case-insensitive, using filename to determine identity.
+            // (This calls out to the `moduleloader` hook, which uses `sails-build-dictionary` and `includeall`
+            //  to `require` and collate the relevant code for these modules-- also adding the appropriate `globalId`
+            //  property.)
+            sails.log.verbose('Loading the app\'s models from `%s`...', sails.config.paths.models);
+            sails.modules.loadModels(function modulesLoaded(err, modules) {
+              if (err) {
+                return cb(err);
               }
 
-              // At this point, we know Mongoose has connected to the database, everything is ready to go, and we can safely trigger `initialize`'s callback.
-              return cb();
-            }//</try>
-            // If anything unexpected happened, pass the error to `initialize`'s callback.
-            catch (e) {
-              return cb(e);
-            }//</try>
-          });//</sails.modules.loadModels>
-        });//</dbConnection.once('open')>
+              try {
+
+                // Instantiate Mongoose schemas for each model definition (running custom `constructSchema` functions if provided)
+                var schemas = _.reduce(modules, function (memo, def, identity) {
+
+                  // Validate `schema` from model def (if omitted, default it to `{}`)
+                  if ( _.isUndefined(def.schema) ){
+                    def.schema = {};
+                  }
+                  if ( !_.isObject(def.schema) || _.isArray(def.schema) ) {
+                    throw new Error('Invalid `schema` provided in model (`'+identity+'`).  If provided, `schema` must be a dictionary.');
+                  }
+
+                  // If no `constructSchema` interceptor function was provided, just new up a Mongoose Schema by passing in `schema` from the model def.
+                  if ( _.isUndefined(def.constructSchema) ) {
+                    memo[identity] = new sails.mongoose.Schema(def.schema);
+                  }
+                  // If `constructSchema` interceptor function WAS provided, run it to get the Schema instance.
+                  else if ( _.isFunction(def.constructSchema) ) {
+                    try {
+                      memo[identity] = def.constructSchema(def.schema, sails);
+                    }
+                    catch (e) {
+                      e.message = 'Encountered an error when running `constructSchema` interceptor provided for model (`'+identity+'`). Details:\n' + e.message;
+                      e.stack = 'Encountered an error when running `constructSchema` interceptor provided for model (`'+identity+'`). Details:\n' + e.stack;
+                      throw e;
+                    }
+                  }
+                  else {
+                    throw new Error('Invalid `constructSchema` interceptor provided in model (`'+identity+'`).  If provided, `constructSchema` must be a function.');
+                  }
+
+
+                  return memo;
+                }, {});
+
+                // Now generate Model constructors from those schemas and expose references to them as `sails.models[identity]`.
+                //
+                // We also set `globalId` and `identity` directly on each Mongoose model.
+                // (this is for consistency with the standard ORM hook, and improved compatibility with any code in other
+                //  community hooks which relies on these properties existing)
+                // Set `globalId` and `identity` directly on the Mongoose model
+                sails.models = _.reduce(schemas, function (memo, mongooseSchemaInstance, identity) {
+                  memo[identity] = sails.mongoose.model(identity, mongooseSchemaInstance);
+                  memo[identity].globalId = modules[identity].globalId;
+                  memo[identity].identity = identity;
+                  return memo;
+                }, {});
+
+
+                // If configured to do so, also expose instantiated models as global variables.
+                // (using `globalId` to expose these models process-wide)
+                if ( _.isObject(sails.config.globals) && sails.config.globals.models ) {
+                  _.each(sails.models, function eachInstantiatedModel(Model, identity) {
+                    // Expose the Model as a global variable.
+                    global[Model.globalId] = Model;
+                  });
+                }
+
+                // At this point, we know Mongoose has connected to the database, everything is ready to go, and we can safely trigger `initialize`'s callback.
+                return cb();
+              }//</try>
+              // If anything unexpected happened, pass the error to `initialize`'s callback.
+              catch (e) {
+                return cb(e);
+              }//</try>
+            });//</sails.modules.loadModels>
+          });//</dbConnection.once('open')>
+        }
+        if (sails.config.mongoose.mockgoose) {
+          mockgoose.prepareStorage().then(function () {
+            connectToDb();
+          });
+        } else {
+          connectToDb();
+        }
+
       }
       // If anything unexpected happened, pass the error to `initialize`'s callback.
       catch (e) {
